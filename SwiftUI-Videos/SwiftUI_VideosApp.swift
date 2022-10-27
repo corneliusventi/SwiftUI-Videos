85065a2852012573bab4fb1f3a72cc62c178584f
Cornelius Venti <corneliusventi@gmail.com>
2022-10-27 17:22:04 +0700

    Initial Commit

---
+++ SwiftUI-Videos/SwiftUI_VideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VideosApp.swift
+//  SwiftUI-Videos
+//
+//  Created by Cornelius Venti on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
